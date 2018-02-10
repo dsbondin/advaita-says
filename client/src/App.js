a1e542653bec33450834b4310d8953f35b48a9b0
github username <github email address>
2018-02-10 16:00:49 -0500

    initialized cors, now both servers are running, able to send fetch request

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fetch from 'isomorphic-fetch'
+
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch("http://localhost:3001/login", {
+      method: "post",
+      body: JSON.stringify({username: "Denis", password: "123"}),
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+      })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
