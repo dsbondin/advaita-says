adb7afdcb51b3b6cf964307719e24f1ff9cf7395
github username <github email address>
2018-02-12 00:59:37 -0500

    added devToolsExtension

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import rootReducer from './reducers/index'
+import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk';
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk)
-)
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
 
 render(
   <Provider store={store}>
