e8572b515c2742aa6f0fff57cbe83b706aa89ac7
github username <github email address>
2018-02-12 01:15:15 -0500

    working on signUp action and auth reducer

--- client/src/actions/signUp.js
+++ client/src/actions/signUp.js
@@ -1,5 +1,6 @@
 export function signUpUser(userData) {
   return (dispatch) => {
+    dispatch({type: "SEND_SIGN_UP_REQUEST"});
     return fetch("http://localhost:3001/users", {
       method: "post",
       body: JSON.stringify(userData),
@@ -10,3 +11,6 @@ export function signUpUser(userData) {
     })
   }
 }
+
+// Finish this first thing in the morning!
+// reducer will set state isLoading to true, on SEND_SIGN_UP_REQUEST, then update username and token upon success
