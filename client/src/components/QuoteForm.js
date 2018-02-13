e746c71b66f11dd45c2a600fec730482248e3953
github username <github email address>
2018-02-13 16:42:46 -0500

    finished new quote form

--- client/src/components/QuoteForm.js
+++ client/src/components/QuoteForm.js
@@ -3,10 +3,54 @@ import { connect } from 'react-redux';
 
 
 class QuoteForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      content: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      content: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.createNewQuote(this.state);
+    this.setState({
+      content: ''
+    });
+  }
+
   render() {
     return (
-      <div>
-        New Quote
+      <div style={{ width: "600px", margin: "auto"}}>
+        <form className="form-signin" onSubmit={this.handleSubmit}>
+          <h2 className="form-signin-heading">Add a new quote</h2>
+          <div class="form-group">
+            <textarea
+              className="form-control"
+              rows="6"
+              value={this.state.content}
+              onChange={this.handleChange}
+              required autoFocus
+            >
+            </textarea>
+          </div>
+
+          <div className="form-group">
+            <button
+              disabled={this.props.isLoading}
+              style={{marginTop: '12px'}}
+              className="btn btn-lg btn-primary btn-block"
+              type="submit"
+            >Submit
+            </button>
+          </div>
+        </form>
       </div>
     )
   }
