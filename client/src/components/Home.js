dfee3dd5174185db3f053d6e0c86585edf5a9653
github username <github email address>
2018-02-14 21:03:45 -0500

    added image and greeting to homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div style={{margin: "20px"}}>
-      <h1>Welcome, stranger!</h1>
-      <p>This little app will display to you some random quotes by Nisargadatta Maharaj.
-      If you like the quote you can save it to your collection. You can also use our form to add a new quote.</p>
+    <div style={{margin: "50px 20px"}}>
+      <img className="pull-left" style={{paddingRight: "20px"}} src={require('../images/Nisargadatta_Maharaj.jpg')} height="320px"/>
+      <h1 style={{textAlign: "center", marginBottom: "20px"}}>Welcome, stranger!</h1>
+      <p style={{paddingLeft: "40px"}}>This little app will display to you some random quotes by <a href="https://en.wikipedia.org/wiki/Nisargadatta_Maharaj">Nisargadatta Maharaj</a>.
+      If you like the quote you can save it to your collection. Or you can add one using
+      the 'New Quote' form.</p>
     </div>
+
   )
 }
 
