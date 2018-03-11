46c66a3aa676c59eb4725fc0148cda17c03125c8
github username <dsbondin@gmail.com>
2018-03-10 19:05:05 -0500

    Add author field to new quote form

--- client/src/containers/QuoteForm.js
+++ client/src/containers/QuoteForm.js
@@ -9,13 +9,28 @@ class QuoteForm extends Component {
     super();
 
     this.state = {
-      content: ''
+      content: '',
+      author: null,
+      authors: []
     }
+  }
+
+  componentWillMount() {
+    this.fetchAuthors();
+  }
+
+  fetchAuthors() {
+    fetch('/api/authors')
+      .then(response => response.json())
+      .then(authors => this.setState({
+        authors: authors
+      })
+    )
   }
 
   handleChange = (event) => {
     this.setState({
-      content: event.target.value
+      [event.target.name]: event.target.value
     })
   }
 
@@ -39,11 +54,21 @@ class QuoteForm extends Component {
               className="form-control"
               style={{fontStyle: "italic", fontSize: "16px", color: "#603"}}
               rows="6"
+              name="content"
               value={this.state.content}
               onChange={this.handleChange}
               required autoFocus
             >
             </textarea>
+          </div>
+
+          <div className="form-group" style={{textAlign: "right"}}>
+            <select name="author" style={{width: "200px"}} onChange={this.handleChange}>
+              <option disabled selected="true">Select author</option>
+              {this.state.authors.map(author => {
+                return <option key={author.id} value={author.id}>{author.name}</option>
+              })}
+            </select>
           </div>
 
           <div className="form-group">
